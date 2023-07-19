7dfb3b17519b83f2123245e57bd5f97d09cd6df8
rey <rey@bbtdigital.com>
2023-07-19 15:12:32 +0800

    [+] auth0 set up

--- src/screens/authentication/login/index.tsx
+++ src/screens/authentication/login/index.tsx
@@ -1,9 +1,9 @@
 // library imports
 import React, { useEffect, useMemo, useState } from "react";
-import { Text, View, Toast } from "react-native-ui-lib";
+import { Text, View, Button } from "react-native-ui-lib";
 import { TouchableOpacity } from "react-native";
 import { register } from "react-native-bundle-splitter";
-
+import { useAuth0, Auth0Provider } from "react-native-auth0";
 // component imports
 import navigationStrings from "../../../navigation/constants/navigationStrings";
 const Form = register<{ getFormParam }>({ loader: () => import("./components/form"), name: "FormComponent" });
@@ -13,16 +13,35 @@ export default function Index(props) {
    const getFormParam = (form) => {
       console.log(form);
    };
-   
+
+   const { authorization } = useAuth0();
+
+   const LoginButton = () => {
+      const { authorize } = useAuth0();
+
+      const onPress = async () => {
+         try {
+            await authorize({ scope: "openid profile email" })
+               .then((e) => console.log(e))
+               .catch((er) => console.log(er, 121212));
+         } catch (e) {
+            console.log(e);
+         }
+      };
+
+      return <Button onPress={onPress} />;
+   };
    return (
-      <View style={{ justifyContent: "center", flex: 1, alignItems: "center" }}>
-         <Text style={{ fontSize: 50 }}>Login screen</Text>
-         <Form getFormParam={getFormParam} />
-         <TouchableOpacity onPress={() => props.navigation.push(navigationStrings.FORGOT_PASSWORD)}>
-            <Text style={{ fontSize: 20, backgroundColor: "yellow", paddingVertical: 5, paddingHorizontal: 5 }}>
-               Forgot password
-            </Text>
-         </TouchableOpacity>
-      </View>
+      <Auth0Provider domain={"dev-mvp8fq05h32j8h2e.us.auth0.com"} clientId={"sSNAtZfk0eObC9QbgcDJeNxar7Bm7tzM"}>
+         <View style={{ justifyContent: "center", flex: 1, alignItems: "center" }}>
+            <Text style={{ fontSize: 50 }}>Login screen</Text>
+            <LoginButton />
+            <TouchableOpacity onPress={() => props.navigation.push(navigationStrings.FORGOT_PASSWORD)}>
+               <Text style={{ fontSize: 20, backgroundColor: "yellow", paddingVertical: 5, paddingHorizontal: 5 }}>
+                  Forgot password
+               </Text>
+            </TouchableOpacity>
+         </View>
+      </Auth0Provider>
    );
 }
